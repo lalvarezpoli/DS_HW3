012273abccc882cae5f5b67f9353d62944200ca9
mgallon235 <125981129+mgallon235@users.noreply.github.com>
2023-10-17 19:29:43 +0200

    update

--- clean_model.py
+++ clean_model.py
@@ -1,12 +1,15 @@
 
+import os
+import pandas as pd
 
-path="/Users/ruimaciel/Desktop/Barcelona/Computing for Data Science/hw3/sample_diabetes_mellitus_data.csv"
+path = os.getcwd()
+path
+df = pd.read_csv(path+'/Notebooks/sample_diabetes_mellitus_data.csv')
 
-df= pd.read_csv(path)
 
 def dropmissingvalues(df):
-    df.dropna(subset=["age", "gender", "ethnicity"])
-    return df
+    data = df.dropna(subset=["age", "gender", "ethnicity"])
+    return data
 
 def fillmissingvalues_with_mean(df):
     df["height"]= df["height"].fillna(df["height"].mean())
@@ -14,9 +17,9 @@ def fillmissingvalues_with_mean(df):
     return df
 
 def inputdummyvariables(df):
-    pd.get_dummies(df, columns=["ethnicity"], prefix="ethnicity")
-    df = df.replace({True: 1, False: 0})
-    return df
+    df2 =pd.get_dummies(df, columns=["ethnicity"], prefix="ethnicity")
+    df2 = df2.replace({True: 1, False: 0})
+    return df2
 
 def model_cleaning(df):
     df1=dropmissingvalues(df)
@@ -24,5 +27,10 @@ def model_cleaning(df):
     df3=inputdummyvariables(df2)
     return df3
 
+df.shape
+df.isna().sum()
 
+df3 = model_cleaning(df)
 
+df3.shape
+df3.isna().sum()
